5e48c3f1d30a9da419cdc50f9ddfd4aee2db14c0
david kitenge emedi <kingbestd030@gmail.com>
2021-10-12 02:26:02 +0200

    ass

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
-$sessionId=$_POST["sessionId"]?$_POST["sessionId"]:"jckbckhvcwhkvhckvwkcv";
-$serviceCode=$_POST["serviceCode"]?$_POST["serviceCode"]:"23423";
-$phoneNumber=$_POST["phoneNumber"]?$_POST["phoneNumber"]:"";
-$text=$_POST["text"]?$_POST["text"]:"3";
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
 if ($text==""){
-    $reponse="Que veux tu?\n";
+    $reponse="CON Que veux tu?\n";
     $reponse.="1. Paiement courent\n";
     $reponse.="2. balance";
 }elseif ($text=="1"){
-    $reponse="Quelle societe?\n";
+    $reponse="CON Quelle societe?\n";
     $reponse.="1. socode\n";
     $reponse.="2. virunga SARL\n";
     $reponse.="3. SNEL";
 }elseif ($text=="1*1" || $text=="1*2" || $text=="1*2"){
-    $reponse="Entre le montant";
+    $reponse="CON Entre le montant";
 }elseif ($text=="1*1*20" || $text=="1*2*20" || $text=="1*2*20"){
-    $reponse="Confiremer votre achat";
+    $reponse="CON Confiremer votre achat";
 }elseif ($text=="1*1*20*1234" || $text=="1*2*20*1234" || $text=="1*2*20*1234"){
 $reponse="END Votre achat et avec succes";
 }
